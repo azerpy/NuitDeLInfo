e4c2014bbd3590110ca47291eecd34d4d430896a
jonathan <pyjonathan31@gmail.com>
2018-12-06 23:32:14 +0100

    init

---
+++ app/src/main/java/m1/rizomm/fges/nuitdelinfo/MainActivity.java
@@ -0,0 +1,13 @@
+package m1.rizomm.fges.nuitdelinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
